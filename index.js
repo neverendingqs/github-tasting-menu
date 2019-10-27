5a1cd5c0aab6fb934994860a55b9da4fdca97d38
Mark Tse <mark.tse@neverendingqs.com>
2019-10-27 16:39:39 -0400

    app - now CC's a user on PR merge.

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+const _ = {
+  get: require('lodash.get')
+};
+
 /**
  * This is the main entrypoint to your Probot app
  * @param {import('probot').Application} app
@@ -6,11 +10,26 @@ module.exports = app => {
   // Your code here
   app.log('Yay, the app was loaded!')
 
-  app.on('issues.opened', async context => {
-    const issueComment = context.issue({ body: 'Thanks for opening this issue!' })
-    return context.github.issues.createComment(issueComment)
-  })
+  app.on(['pull_request.closed'], async context => {
+    if(!_.get(context, 'payload.pull_request.merged', true)) {
+      return Promise.resolve();
+    }
 
+    const userToNotify = 'neverendingqs';
+
+    const response = await context.github.repos.checkCollaborator({
+      owner: context.payload.repository.owner.login,
+      repo: context.payload.repository.name,
+      username: userToNotify
+    });
+
+    if(response.status !== 204) {
+      return Promise.resolve();
+    }
+
+    const issueComment = context.issue({ body: `cc: @${userToNotify}` });
+    return context.github.issues.createComment(issueComment);
+  })
   // For more information on building apps:
   // https://probot.github.io/docs/
 
