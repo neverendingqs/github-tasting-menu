e44f6c458586c974a710a8b34ff6d7d840e5634f
Mark Tse <mark.tse@neverendingqs.com>
2019-10-27 21:37:15 -0400

    app - now gets frequencies from file.

--- index.js
+++ index.js
@@ -2,12 +2,63 @@ const _ = {
   get: require('lodash.get')
 };
 
+async function getConfig(context) {
+  const config = await context.config('tasting-menu.yml');
+  if(!config) { return Promise.resolve(); }
+  return config;
+}
+
+function getUsersToNotify(frequencies) {
+  return frequencies
+    .filter(({ frequency }) => Math.random() < frequency)
+    .map(({ username }) => username);
+}
+
+async function verifyUsers(context, users) {
+  const collaborators = [];
+  const nonCollaborators = [];
+
+  for(const username of users) {
+    try {
+      const response = await context.github.repos.checkCollaborator({
+        owner: context.payload.repository.owner.login,
+        repo: context.payload.repository.name,
+        username
+      });
+
+      switch(response.status) {
+        case 204:
+          collaborators.push(username);
+          break;
+        default:
+          nonCollaborators.push(username);
+      }
+    } catch {
+      nonCollaborators.push(username);
+    }
+  }
+
+  return { collaborators, nonCollaborators };
+}
+
+function constructBody(collaborators, nonCollaborators) {
+  let body = '';
+  if(collaborators.length > 0) {
+    body += `cc: ${collaborators.map(username => `@${username}`).join(' ')}\n\n`;
+  }
+
+  if(nonCollaborators.length > 0) {
+    body += `Non-collaborators / non-existent users that were not notified: \n\n - ${nonCollaborators.join('\n - ')}`;
+  }
+
+  return body;
+}
+
 /**
  * This is the main entrypoint to your Probot app
  * @param {import('probot').Application} app
  */
 module.exports = app => {
-  // Your code here
   app.log('Yay, the app was loaded!')
 
   app.on(['pull_request.closed'], async context => {
@@ -15,24 +66,17 @@ module.exports = app => {
       return Promise.resolve();
     }
 
-    const userToNotify = 'neverendingqs';
+    const config = await getConfig(context);
+    if(!config) { return Promise.resolve(); }
 
-    const response = await context.github.repos.checkCollaborator({
-      owner: context.payload.repository.owner.login,
-      repo: context.payload.repository.name,
-      username: userToNotify
-    });
+    const frequencies = _.get(config, 'pull_request.merged');
+    if(!frequencies) { return Promise.resolve(); }
 
-    if(response.status !== 204) {
-      return Promise.resolve();
-    }
+    const users = getUsersToNotify(frequencies);
+    const { collaborators, nonCollaborators } = await verifyUsers(context, users);
+    const body = constructBody(collaborators, nonCollaborators) || 'No collaborators were chosen for this pull request.';
 
-    const issueComment = context.issue({ body: `cc: @${userToNotify}` });
+    const issueComment = context.issue({ body });
     return context.github.issues.createComment(issueComment);
-  })
-  // For more information on building apps:
-  // https://probot.github.io/docs/
-
-  // To get your app running against GitHub, see:
-  // https://probot.github.io/docs/development/
+  });
 }
