575fcd22f350a79dcb5b53eadd504818235e192d
Mark Tse <mark.tse@neverendingqs.com>
2019-10-29 21:12:51 -0400

    linting.

--- now.js
+++ now.js
@@ -1,3 +1,3 @@
-const { serverless } = require('@chadfawcett/probot-serverless-now');
-const appFn = require('./');
-module.exports = serverless(appFn);
+const { serverless } = require('@chadfawcett/probot-serverless-now')
+const appFn = require('./')
+module.exports = serverless(appFn)
