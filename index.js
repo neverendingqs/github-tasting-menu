ef3265da388af49d084e1e2825f7610844c2c9a8
Mark Tse <neverendingqs@users.noreply.github.com>
2019-10-29 21:39:46 -0400

    now - init. (#2)
    * now - init.
    
    * now - adding secrets.
    
    * now - adding builds and routes.
    
    * now - trying something else.
    
    * now - adding peer dep.
    
    * now - trying another package.
    
    * empty commit.
    
    * updating to base64.
    
    * bumping engines.
    
    * trying without engines.
    
    * reverting engines.
    
    * trying now-build.
    
    * Revert "trying now-build."
    
    This reverts commit 4b8cb3840b0897ee6a4bc0f86e9032fbe35c6f6b.
    
    * empty commit.
    
    * empty commit.
    
    * circleci - init.
    
    * testing circleci deploy.
    
    * linting.
    
    * removing unused.
    
    * adding name.
    
    * re-adding master filter for deploys.
    
    * stop npm from showing secret.
    
    * test that npm stop showing secrets.
    
    * Revert "test that npm stop showing secrets."
    
    This reverts commit 52516b869bd25f2cfb9a33286db6800f824e75b0.

--- index.js
+++ index.js
@@ -1,57 +1,57 @@
 const _ = {
   get: require('lodash.get')
-};
-
-async function getConfig(context) {
-  const config = await context.config('tasting-menu.yml');
-  if(!config) { return Promise.resolve(); }
-  return config;
 }
 
-function getUsersToNotify(frequencies) {
+async function getConfig (context) {
+  const config = await context.config('tasting-menu.yml')
+  if (!config) { return Promise.resolve() }
+  return config
+}
+
+function getUsersToNotify (frequencies) {
   return frequencies
     .filter(({ frequency }) => Math.random() < frequency)
-    .map(({ username }) => username);
+    .map(({ username }) => username)
 }
 
-async function verifyUsers(context, users) {
-  const collaborators = [];
-  const nonCollaborators = [];
+async function verifyUsers (context, users) {
+  const collaborators = []
+  const nonCollaborators = []
 
-  for(const username of users) {
+  for (const username of users) {
     try {
       const response = await context.github.repos.checkCollaborator({
         owner: context.payload.repository.owner.login,
         repo: context.payload.repository.name,
         username
-      });
+      })
 
-      switch(response.status) {
+      switch (response.status) {
         case 204:
-          collaborators.push(username);
-          break;
+          collaborators.push(username)
+          break
         default:
-          nonCollaborators.push(username);
+          nonCollaborators.push(username)
       }
-    } catch {
-      nonCollaborators.push(username);
+    } catch (err) {
+      nonCollaborators.push(username)
     }
   }
 
-  return { collaborators, nonCollaborators };
+  return { collaborators, nonCollaborators }
 }
 
-function constructBody(collaborators, nonCollaborators) {
-  let body = '';
-  if(collaborators.length > 0) {
-    body += `cc: ${collaborators.map(username => `@${username}`).join(' ')}\n\n`;
+function constructBody (collaborators, nonCollaborators) {
+  let body = ''
+  if (collaborators.length > 0) {
+    body += `cc: ${collaborators.map(username => `@${username}`).join(' ')}\n\n`
   }
 
-  if(nonCollaborators.length > 0) {
-    body += `Non-collaborators / non-existent users that were not notified: \n\n - ${nonCollaborators.join('\n - ')}`;
+  if (nonCollaborators.length > 0) {
+    body += `Non-collaborators / non-existent users that were not notified: \n\n - ${nonCollaborators.join('\n - ')}`
   }
 
-  return body;
+  return body
 }
 
 /**
@@ -62,21 +62,21 @@ module.exports = app => {
   app.log('Yay, the app was loaded!')
 
   app.on(['pull_request.closed'], async context => {
-    if(!_.get(context, 'payload.pull_request.merged', true)) {
-      return Promise.resolve();
+    if (!_.get(context, 'payload.pull_request.merged', true)) {
+      return Promise.resolve()
     }
 
-    const config = await getConfig(context);
-    if(!config) { return Promise.resolve(); }
+    const config = await getConfig(context)
+    if (!config) { return Promise.resolve() }
 
-    const frequencies = _.get(config, 'pull_request.merged');
-    if(!frequencies) { return Promise.resolve(); }
+    const frequencies = _.get(config, 'pull_request.merged')
+    if (!frequencies) { return Promise.resolve() }
 
-    const users = getUsersToNotify(frequencies);
-    const { collaborators, nonCollaborators } = await verifyUsers(context, users);
-    const body = constructBody(collaborators, nonCollaborators) || 'No collaborators were chosen for this pull request.';
+    const users = getUsersToNotify(frequencies)
+    const { collaborators, nonCollaborators } = await verifyUsers(context, users)
+    const body = constructBody(collaborators, nonCollaborators) || 'No collaborators were chosen for this pull request.'
 
-    const issueComment = context.issue({ body });
-    return context.github.issues.createComment(issueComment);
-  });
+    const issueComment = context.issue({ body })
+    return context.github.issues.createComment(issueComment)
+  })
 }
