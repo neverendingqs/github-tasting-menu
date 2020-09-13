605d182bb7b1818b98fe9e89e74c4f2ec7f21173
Mark Tse <mark.tse@neverendingqs.com>
2020-09-13 17:19:47 -0400

    fix: helper function has moved.

--- now.js
+++ now.js
@@ -1,3 +1,13 @@
-const { serverless } = require('@chadfawcett/probot-serverless-now')
-const appFn = require('./')
-module.exports = serverless(appFn)
+// Based on https://github.com/chadfawcett/probot-serverless-now/blob/master/index.js
+const { createProbot } = require('probot')
+const { findPrivateKey } = require('probot/lib/helpers/get-private-key')
+
+const app = require('./')
+const probot = createProbot({
+  id: process.env.APP_ID,
+  secret: process.env.WEBHOOK_SECRET,
+  cert: findPrivateKey()
+})
+probot.load(app)
+
+module.exports = probot.server
