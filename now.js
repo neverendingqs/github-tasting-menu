c3f17d900b1ae30f832fe063d227e18410a76f5b
Mark Tse <mark.tse@neverendingqs.com>
2020-12-22 13:47:34 -0500

    fix: using new @probot/get-private-key library.

--- now.js
+++ now.js
@@ -1,12 +1,12 @@
 // Based on https://github.com/chadfawcett/probot-serverless-now/blob/master/index.js
 const { createProbot } = require('probot')
-const { findPrivateKey } = require('probot/lib/helpers/get-private-key')
+const { getPrivateKey } = require('@probot/get-private-key')
 
 const app = require('./')
 const probot = createProbot({
   id: process.env.APP_ID,
   secret: process.env.WEBHOOK_SECRET,
-  cert: findPrivateKey()
+  cert: getPrivateKey()
 })
 probot.load(app)
 
