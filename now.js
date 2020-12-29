ba80d8d8b3c75e0e06f0fa36efd7808d9f952047
Mark Tse <mark.tse@neverendingqs.com>
2020-12-28 21:11:24 -0500

    chore: update deps. (#51)
    * Revert "Revert "chore: update deps (2020-12-22) (#48)" (#50)"
    
    This reverts commit 9e58831c474fc1a9da9ab868cc4769cfea2dd8e4.
    
    * chore: updating vercel integration.

--- now.js
+++ now.js
@@ -1,13 +1,4 @@
-// Based on https://github.com/chadfawcett/probot-serverless-now/blob/master/index.js
-const { createProbot } = require('probot')
-const { getPrivateKey } = require('@probot/get-private-key')
+const { createNodeMiddleware, createProbot } = require('probot')
 
 const app = require('./')
-const probot = createProbot({
-  id: process.env.APP_ID,
-  secret: process.env.WEBHOOK_SECRET,
-  cert: getPrivateKey()
-})
-probot.load(app)
-
-module.exports = probot.server
+module.exports = createNodeMiddleware(app, createProbot())
